b60d8e4a765879d6be85acd13a74a1eea3e03b13
Facundo Pettersson <petterssonfacundo@gmail.com>
2024-03-23 14:54:03 -0300

    Informacion del JSX

--- react_study/src/App.jsx
+++ react_study/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const nombre = <h1>Facundo</h1>
+  const edad = <h2>19</h2>
+  
+  const usurio = <div>
+    {nombre}{edad}
+  </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <>
+    <div>
+      <MostrarNombreComponente />
+      <MostrarNombreComponente />
+      <MostrarNombreComponente />
+    </div>
+
+  </>;
 }
 
-export default App
+
+
+export const MostrarNombreComponente =()=>{
+  return <h1>Hola Facundo</h1>
+}
+
+
+
+
+export default App;
