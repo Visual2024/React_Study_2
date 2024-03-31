e2c87d3965a11bf136b180d96816400b0ecbe1ef
Facundo Pettersson <petterssonfacundo@gmail.com>
2024-03-31 18:22:41 -0300

    Compilacion

--- react_study/src/App.jsx
+++ react_study/src/App.jsx
@@ -1,20 +1,10 @@
 import "./App.css";
-<<<<<<< HEAD
-
-
-=======
 import { ItemListContainer } from "./Components/items/ItemListContainer";
->>>>>>> clase-5_Promesas
-function App() {
-  
 
+function App() {
   return (   
     <>
-<<<<<<< HEAD
-      
-=======
       <ItemListContainer />
->>>>>>> clase-5_Promesas
     </>
   );
 }
